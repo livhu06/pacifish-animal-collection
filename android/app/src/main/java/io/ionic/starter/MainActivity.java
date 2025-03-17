7a48447675528cccd15ad9965587ccfc15ef498a
livhu06 <livhuwaninkuna@ymail.com>
2025-03-17 22:25:13 +0200

    added firestore database

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,46 @@
 package io.ionic.starter;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.annotation.NonNull;
+
+import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends BridgeActivity {
+
+  DatabaseReference databaseReference;
+
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    databaseReference= FirebaseDatabase.getInstance().getReference("This is the path");
+    databaseReference.setValue("Hi there").addOnSuccessListener(new OnSuccessListener<Void>() {
+      @Override
+      public void onSuccess(Void unused) {
+        Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+
+      }
+    }).addOnFailureListener(new OnFailureListener() {
+      @Override
+      public void onFailure(@NonNull Exception e) {
+        Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+
+      }
+    }).addOnCompleteListener(new OnCompleteListener<Void>() {
+      @Override
+      public void onComplete(@NonNull Task<Void> task) {
+
+      }
+    });
+
+    }
+
+  }
